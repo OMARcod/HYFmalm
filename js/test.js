1cfdd3aa6cbe8cd554c06387c4fc961aec1762a7
OMARcod <31794534+OMARcod@users.noreply.github.com>
2018-10-09 16:04:04 +0200

    s

---
+++ js/test.js
@@ -0,0 +1,6 @@
+var number = 10;
+if (number < 20) {
+ert("this alworking");  
+}
+// here can u type som word
+document.body.innerHTML = "oallo world is working";
--- test.js
+++
@@ -1,6 +0,0 @@
-var number = 10;
-if (number > 20) {
-alert("this working");  
-}
-
-document.body.innerHTML = "oallo world is working";
