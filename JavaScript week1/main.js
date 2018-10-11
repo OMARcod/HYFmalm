d6121056de6cf3350332d463c2a38644ea5c5d6d
OMARcod <31794534+OMARcod@users.noreply.github.com>
2018-10-11 15:20:49 +0200

    s
    s

---
+++ JavaScript week1/main.js
@@ -0,0 +1,100 @@
+// 1
+console.log("Hallo World!");
+console.log("Ciao mondo!");
+console.log("Hola mundo!");
+
+// 2 theres tow way to solv that problem
+console.log('I\'m awesome');
+console.log("I'm awesome");
+
+
+// 3
+var x;
+console.log("the value of x will be: 10");
+console.log(x);
+x = 10;
+console.log('I think the value of x is 10 ');
+console.log('the real value is: ' + x);
+
+// 4
+var y;
+console.log('the value of my string will be: OMAR');
+console.log(y)
+y = "DODO";
+console.log("I think the value of y is DODO");
+console.log(y);
+
+// 5
+var z = 7.25;
+console.log(z);
+var a =  Math.round(z);
+console.log(a);
+console.log(Math.max(z, a));
+
+// 6 
+var arrays = [];
+console.log("I think array is empty")
+console.log(arrays);
+var arrays = ["cat" , "dog" , "rabbit"];
+console.log(arrays);
+arrays.push("horse");
+console.log(arrays);
+
+// 7
+let myString = "this is a test";
+console.log(myString);
+console.log(myString.length);
+
+// 8
+let var1 = 3;
+console.log('The value of my variable var1 is: ' + var1);
+let var2 = true;
+console.log('The value of my variable var2 is: ' + var2);
+let var3 = [5 , 2 , 1];
+console.log('The value of my variable var3 is: ' + var3 );
+let var4 = "omar";
+console.log('The value of my variable var4 is: ' + var4);
+
+console.log("I think the type of var1 is: Number");
+console.log("I think the type of var2 is: Boolean");
+console.log("I think the type of var3 is: Array");
+console.log("I think the type of var4 is: String");
+
+console.log(typeof(var1));
+console.log(typeof(var2));
+console.log(typeof(var3));
+console.log(typeof(var4));
+console.log(typeof(var5));
+
+if (var1 == var2) {
+ console.log(var1 + " and "+ var2 + " dont have the same type of value");
+} else {
+ console.log(var1 + " and "+ var2+ " dont have the same value");
+};
+
+if (var2 == var3) {
+ console.log(var2 + " and "+ var3 + " dont have the same type of value");
+} else {
+ console.log(var2 + " and "+ var3+ " dont have the same value");
+};
+
+if (var3 == var4) {
+ console.log(var3 + " and "+ var4 + " dont have the same type of value");
+} else {
+ console.log(var3 + " and "+ var4+ " dont have the same value");
+};
+
+if (var4 == var1) {
+ console.log(var4 + " and "+ var1 + " dont have the same type of value");
+} else {
+ console.log(var4 + " and "+ var1+ " dont have the same value");
+};
+
+// 9 i did not understand this part i looked how other did 
+var x = 7;
+console.log('The value of ' + x + ' % ' + 3 + ' is: ' + (x % 3));
+console.log('The value of (10 % 2) is: ' + 10 % 2);
+console.log('The value of (16 % 6 ) is: ' + 16 % 6);
+console.log('The value of (29 % 10) is: ' + 29 % 10);
+
+// 10
