e2343d4998e61dc477f99aa5aecfac30391a80c7
Omar Abdallah <omarzk2011@gmail.com>
2018-10-13 21:18:20 +0200

    s
    s

--- JsWeaK2/weak2.js
+++ JsWeaK2/weak2.js
@@ -1,44 +1,45 @@
-// // 1 Strings!
-// let myString = "hello,this,is,a,difficult,to,read,sentence";
-// console.log(myString);
-// console.log(myString.length);
-// myString = myString.replace(/[, ]+/g, " ").trim();
-// console.log(myString);
+// 1 Strings!
+let myString = "hello,this,is,a,difficult,to,read,sentence";
+console.log(myString);
+console.log(myString.length);
+myString = myString.replace(/[, ]+/g, " ").trim();
+console.log(myString);
 
-// // 2 Arrays!
-// let favoriteAnimals = ['blowfish', 'capricorn', 'giraffe'];
-// favoriteAnimals.push("turtle");
-// console.log(favoriteAnimals);
-// // att index 1 delet 0 element and add "meerkat"
-// favoriteAnimals.splice( 1, 0, "meerkat");
-// console.log(favoriteAnimals);
-// console.log("The array has a length of: "+favoriteAnimals.length);
-// favoriteAnimals.splice( 3, 1);
-// console.log(favoriteAnimals);
+// 2 Arrays!
+let favoriteAnimals = ['blowfish', 'capricorn', 'giraffe'];
+favoriteAnimals.push("turtle");
+console.log(favoriteAnimals);
+// att index 1 delet 0 element and add "meerkat"
+favoriteAnimals.splice( 1, 0, "meerkat");
+console.log(favoriteAnimals);
+console.log("The array has a length of: "+favoriteAnimals.length);
+favoriteAnimals.splice( 3, 1);
+console.log(favoriteAnimals);
 
-// var itemX = "meerkat";
-// for (var i = 0; i <= favoriteAnimals.length ; i++) {
-//  if (itemX == favoriteAnimals[i]) {
-//   console.log("The item (" + itemX + ") you are looking for is at index: " + i)
-//  };
-// };
+var itemX = "meerkat";
+for (var i = 0; i <= favoriteAnimals.length ; i++) {
+ if (itemX == favoriteAnimals[i]) {
+  console.log("The item (" + itemX + ") you are looking for is at index: " + i)
+ };
+};
 
 
-// // more JavaScript 1
-// function arguments(first , secund, therd) {
-//  console.log(first + " " + secund + " " + therd);
-// }
+// more JavaScript 1
+function arguments(first , secund, therd) {
+ console.log(first + " " + secund + " " + therd);
+}
 
-// arguments("what did u do" , "last summer" , 123);
+arguments("what did u do" , "last summer" , 123);
 
-// // colorCar
-// function colorCar(color) {
+// colorCar
+function colorCar(color) {
 
-//  console.log("a "+ color + " car.");
+ console.log("a "+ color + " car.");
  
-// };
-// colorCar("red");
+};
+colorCar("red");
 
+// 3
 var theKing = {
  first_name: "The",
  last_name: "King",
