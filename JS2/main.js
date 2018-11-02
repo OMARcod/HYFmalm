ff5567376283ad104cf369c932afd460aabfca69
Omar Abdallah <omarzk2011@gmail.com>
2018-11-02 22:34:37 +0100

    f
    f

---
+++ JS2/main.js
@@ -0,0 +1,3 @@
+ let arr = ["harry_potter" ,"harry_lotter" ,"barry_potter" ,"tarry_potter" ,"harsy_potter" ,"harry_potter2" ,"" ,"" ,"" ,""];
+console.log(arr)
+console.log("what is your name " + arr[0]);
